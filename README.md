d1e02137bc8b27fbbc9ee213b252f6567b1182a0
Hunter Chang <hunter@madwiremedia.com>
2018-09-04 16:01:04 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# gatsby-starter-dimension-v2
+Gatsby.js V2 starter based on the Dimension site template, designed by HTML5 UP. Check out https://codebushi.com/gatsby-starters/ for more Gatsby starters and templates.
+
+## Preview
+
+http://gatsby-dimension-v2.surge.sh/
+
+## Installation
+
+Install this starter (assuming Gatsby is installed) by running from your CLI:
+`gatsby new gatsby-starter-dimension https://github.com/codebushi/gatsby-starter-dimension-v2`
+
+Run `gatsby develop` in the terminal to start the dev site.
---
+++ src/pages/index.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
+
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isArticleVisible: false,
+      timeout: false,
+      articleTimeout: false,
+      article: '',
+      loading: 'is-loading'
+    }
+    this.handleOpenArticle = this.handleOpenArticle.bind(this)
+    this.handleCloseArticle = this.handleCloseArticle.bind(this)
+  }
+
+  componentDidMount () {
+    this.timeoutId = setTimeout(() => {
+        this.setState({loading: ''});
+    }, 100);
+  }
+
+  componentWillUnmount () {
+    if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+    }
+  }
+
+  handleOpenArticle(article) {
+
+    this.setState({
+      isArticleVisible: !this.state.isArticleVisible,
+      article
+    })
+
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout
+      })
+    }, 325)
+
+    setTimeout(() => {
+      this.setState({
+        articleTimeout: !this.state.articleTimeout
+      })
+    }, 350)
+
+  }
+
+  handleCloseArticle() {
+
+    this.setState({
+      articleTimeout: !this.state.articleTimeout
+    })
+
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout
+      })
+    }, 325)
+
+    setTimeout(() => {
+      this.setState({
+        isArticleVisible: !this.state.isArticleVisible,
+        article: ''
+      })
+    }, 350)
+
+  }
+
+  render() {
+    return (
+      <Layout location={this.props.location}>
+        <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
+          <div id="wrapper">
+            <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
+            <Main
+              isArticleVisible={this.state.isArticleVisible}
+              timeout={this.state.timeout}
+              articleTimeout={this.state.articleTimeout}
+              article={this.state.article}
+              onCloseArticle={this.handleCloseArticle}
+            />
+            <Footer timeout={this.state.timeout} />
+          </div>
+          <div id="bg"></div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default IndexPage
